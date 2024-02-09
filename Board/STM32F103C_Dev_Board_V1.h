8d3493ebcbffa342ceffa3acdb97544d66694342
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-02-09 23:59:58 +0800

    Happy Chinese Loong Year!!!!!
    Further wrap the Dev_Board module

--- Board/STM32F103C_Dev_Board_V1.h
+++ Board/STM32F103C_Dev_Board_V1.h
@@ -5,6 +5,13 @@
 
 //STM32F103C_Dev_Board_V1.h
 
+#define Board_SPI_SCK_GPIO	GPIOA
+#define Board_SPI_SCK_Pin	GPIO_Pin_7
+#define Board_SPI_SDA_GPIO	GPIOA
+#define Board_SPI_SDA_Pin	GPIO_Pin_5
+#define Board_SPI_DC_GPIO	GPIOB
+#define Board_SPI_DC_Pin	GPIO_Pin_10
+
 extern OLED_SPI OLED1;
 extern OLED_SPI OLED2;
 extern OLED_SPI OLED3;
