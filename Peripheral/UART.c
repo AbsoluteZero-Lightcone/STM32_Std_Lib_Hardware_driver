47f0cf4d6a8be934bc106ab8dbe721ec1a14ef85
Lightcone <AbsoluteZeroStudio@outlook.com>
2024-03-15 19:44:01 +0800

    fix RCC settings and GPIO settings

--- Peripheral/UART.c
+++ Peripheral/UART.c
@@ -10,14 +10,14 @@
 #include "STM32Device.h"
 void Serial_Init(){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
-	
+	SimpleConfigGPIO(USART1_TX_AF_GPIO,USART1_TX_AF_Pin,GPIO_Mode_AF_PP);
 	USART_InitTypeDef USART_InitStruct = {
 		.USART_BaudRate            = 9600,
 		.USART_WordLength          = USART_WordLength_8b,
 		.USART_StopBits            = USART_StopBits_1,
 		.USART_Parity              = USART_Parity_No,
 		.USART_Mode                = USART_Mode_Tx,
-		.USART_HardwareFlowControl = USART_HardwareFlowControl_None,
+		.USART_HardwareFlowControl = USART_HardwareFlowControl_None
 	};
 	USART_Init(USART1,&USART_InitStruct);
 	USART_Cmd(USART1,ENABLE);
